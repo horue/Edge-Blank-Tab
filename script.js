11f2f8e79c91d87697bbe4e08b1dea17c266ad25
Jorge <jorgejapao00@gmail.com>
2025-06-11 13:12:17 -0300

    now checks browser theme

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-    const userLang = navigator.language || navigator.userLanguage;
-    const mensagemDiv = document.getElementById('mensagem');
+const userLang = navigator.language || navigator.userLanguage;
+const mensagemDiv = document.getElementById('mensagem');
 
-    if (userLang.startsWith('pt')) {
-      document.title = 'Nova guia';
-    } else if (userLang.startsWith('en')) {
-      document.title = 'New tab';
-    } else {
-      document.title = 'New tab';
-    }
+if (userLang.startsWith('pt')) {
+    document.title = 'Nova guia';
+} else if (userLang.startsWith('en')) {
+    document.title = 'New tab';
+} else {
+    document.title = 'New tab';
+}
+
+const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if (isDarkMode) {
+    document.body.style.backgroundColor = '#242424'; 
+} else {
+    document.body.style.backgroundColor = '#ffffff'; 
+}
